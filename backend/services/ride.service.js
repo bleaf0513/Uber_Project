629f293ba15fa771de51185fbde6832103e3408c
pablobedoyab-boop <pablo.bedoyab@gmail.com>
2026-04-08 18:03:05 -0400

    Update ride.service.js

--- backend/services/ride.service.js
+++ backend/services/ride.service.js
@@ -1,15 +1,3 @@
-const rideModel = require("../models/ride.model");
-const mapService = require("../services/maps.service");
-const crypto = require("crypto");
-const userModel = require("../models/user.model");
-
-function getOtp(num) {
-    function generateOtp(num) {
-        return crypto.randomInt(Math.pow(10, num - 1), Math.pow(10, num)).toString();
-    }
-    return generateOtp(num);
-}
-
 const getFare = async (pickup, destination) => {
     if (!pickup || !destination) {
         throw new Error("pickup and destination are required");
@@ -26,7 +14,6 @@ const getFare = async (pickup, destination) => {
     const distanceKm = meters / 1000;
     const durationMin = seconds / 60;
 
-    // Tarifas pensadas para COP
     const baseFare = {
         car: 3500,
         moto: 2200,
@@ -74,129 +61,4 @@ const getFare = async (pickup, destination) => {
         moto: Math.max(fares.moto, minimumFare.moto),
         auto: Math.max(fares.auto, minimumFare.auto),
     };
-};
-
-module.exports.getFare = getFare;
-
-module.exports.createRide = async ({ user, pickup, destination, vehicle }) => {
-    if (!user || !pickup || !destination || !vehicle) {
-        throw new Error("All fields are required");
-    }
-
-    try {
-        const latestUser = await userModel.findById(user._id);
-        const fares = await getFare(pickup, destination);
-
-        const ride = new rideModel({
-            user: latestUser,
-            pickup: pickup,
-            destination: destination,
-            otp: getOtp(6),
-            fare: fares[vehicle],
-            vehicle: vehicle,
-        });
-
-        await ride.save();
-        return ride;
-    } catch (error) {
-        console.error("Error creating ride:", error);
-        throw new Error("Failed to create ride: " + error.message);
-    }
-};
-
-module.exports.confirmRide = async ({ rideId, captain }) => {
-    if (!rideId) {
-        throw new Error("Ride id is required");
-    }
-
-    await rideModel.findOneAndUpdate(
-        { _id: rideId },
-        {
-            status: "accepted",
-            captain: captain._id,
-        }
-    );
-
-    const ride = await rideModel.findOne({ _id: rideId })
-        .populate("user")
-        .populate("captain")
-        .select("+otp");
-
-    if (!ride) {
-        throw new Error("Ride not found");
-    }
-
-    const latestUser = await userModel.findById(ride.user._id);
-    ride.user = latestUser;
-
-    return ride;
-};
-
-module.exports.startRide = async ({ rideId, otp, captain }) => {
-    if (!rideId || !otp) {
-        throw new Error("Ride id and OTP are required");
-    }
-
-    const ride = await rideModel.findOne({ _id: rideId })
-        .populate("user")
-        .populate("captain")
-        .select("+otp");
-
-    if (!ride) {
-        throw new Error("Ride not found");
-    }
-
-    if (ride.status !== "accepted") {
-        throw new Error("Ride not accepted");
-    }
-
-    if (ride.otp !== otp) {
-        throw new Error("Invalid OTP");
-    }
-
-    const latestUser = await userModel.findById(ride.user._id);
-    ride.user = latestUser;
-
-    await rideModel.findOneAndUpdate(
-        { _id: rideId },
-        {
-            status: "ongoing",
-        }
-    );
-
-    return ride;
-};
-
-module.exports.endRide = async ({ rideId, captain }) => {
-    if (!rideId) {
-        throw new Error("Ride id is required");
-    }
-
-    const ride = await rideModel.findOne({
-        _id: rideId,
-        captain: captain._id,
-    })
-        .populate("user")
-        .populate("captain")
-        .select("+otp");
-
-    if (!ride) {
-        throw new Error("Ride not found"); 
-    }
-
-    if (ride.status !== "ongoing") {
-        throw new Error("Ride not ongoing");
-    }
-
-    const latestUser = await userModel.findById(ride.user._id);
-    ride.user = latestUser;
-
-    await rideModel.findOneAndUpdate(
-        { _id: rideId },
-        {
-            status: "completed",
-        }
-    );
-
-    return ride;
 };
