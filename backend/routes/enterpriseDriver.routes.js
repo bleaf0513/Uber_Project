242c5f251ca5ecf21f51f1590553c4f468e9c26c
pablobedoyab-boop <pablo.bedoyab@gmail.com>
2026-04-08 23:08:21 -0400

    Update enterpriseDriver.routes.js

--- backend/routes/enterpriseDriver.routes.js
+++ backend/routes/enterpriseDriver.routes.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const enterpriseDriverController = require('../controllers/enterpriseDriver.controller');
 
-router.get('/', enterpriseDriverController.getDrivers);
-router.post('/', enterpriseDriverController.createDriver);
-router.post('/login', enterpriseDriverController.loginDriverByCedula);
-router.delete('/:id', enterpriseDriverController.deleteDriver);
+const authEnterprise = require('../middlewares/authEnterprise');
+const {
+  createDriver,
+  getDrivers,
+  loginDriverByCedula,
+  deleteDriver,
+} = require('../controllers/enterpriseDriver.controller');
+
+router.get('/', authEnterprise, getDrivers);
+router.post('/', authEnterprise, createDriver);
+router.delete('/:id', authEnterprise, deleteDriver);
+
+// este sí puede quedar sin authEnterprise si es acceso por cédula del conductor
+router.post('/login', loginDriverByCedula);
 
 module.exports = router;
