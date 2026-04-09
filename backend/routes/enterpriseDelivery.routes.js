06915e5bc0db58021968ee81b3aeb39a947b9ee1
pablobedoyab-boop <pablo.bedoyab@gmail.com>
2026-04-09 00:32:22 -0400

    Update enterpriseDelivery.routes.js

--- backend/routes/enterpriseDelivery.routes.js
+++ backend/routes/enterpriseDelivery.routes.js
@@ -2,13 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const authEnterprise = require('../middlewares/authEnterprise');
+const authEnterpriseDriver = require('../middlewares/authEnterpriseDriver');
+
 const {
     getEnterpriseDeliveries,
+    getMyEnterpriseDeliveries,
     createEnterpriseDelivery,
     deleteEnterpriseDelivery,
 } = require('../controllers/enterpriseDelivery.controller');
 
 router.get('/', authEnterprise, getEnterpriseDeliveries);
+router.get('/me', authEnterpriseDriver, getMyEnterpriseDeliveries);
 router.post('/', authEnterprise, createEnterpriseDelivery);
 router.delete('/:id', authEnterprise, deleteEnterpriseDelivery);
 
