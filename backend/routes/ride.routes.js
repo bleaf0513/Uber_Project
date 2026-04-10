e2adf96f2f301642c72a379e2d1a932a53c5721b
pablobedoyab-boop <pablo.bedoyab@gmail.com>
2026-04-10 17:38:54 -0400

    Update ride.routes.js

--- backend/routes/ride.routes.js
+++ backend/routes/ride.routes.js
@@ -3,30 +3,51 @@ const router = express.Router();
 const rideController = require('../controllers/ride.controller');
 const { body, query } = require('express-validator');
 const authMiddleware = require('../middlewares/auth.middleware');
-router.post('/create',
+
+router.post(
+    '/create',
     authMiddleware.authUser,
-    body('pickup').isString().notEmpty().isLength({ min: 3 }).withMessage('Invalid Pickup Address'),
-    body('destination').isString().notEmpty().isLength({ min: 3 }).withMessage('Invalid Destination Address'),
-    body('vehicle').isString().notEmpty().isIn(['auto', 'car', 'moto']).withMessage('Invalid Vehicle Type'),
+    body('pickup')
+        .isString()
+        .notEmpty()
+        .isLength({ min: 3 })
+        .withMessage('Invalid Pickup Address'),
+    body('destination')
+        .isString()
+        .notEmpty()
+        .isLength({ min: 3 })
+        .withMessage('Invalid Destination Address'),
+    body('vehicle')
+        .isString()
+        .notEmpty()
+        .isIn(['motorcycle', 'car', 'light_cargo', 'van', 'truck'])
+        .withMessage('Invalid Vehicle Type'),
     rideController.createRide
-)
-router.post('/confirm',
+);
+
+router.post(
+    '/confirm',
     authMiddleware.authCaptain,
     body('rideId').isMongoId().withMessage('Invalid ride id'),
     rideController.confirmRide
-)
+);
 
-router.get('/start-ride',
+router.get(
+    '/start-ride',
     authMiddleware.authCaptain,
     query('rideId').isMongoId().withMessage('Invalid ride id'),
-    query('otp').isString().isLength({ min: 6, max: 6 }).withMessage('Invalid OTP'),
+    query('otp')
+        .isString()
+        .isLength({ min: 6, max: 6 })
+        .withMessage('Invalid OTP'),
     rideController.startRide
-)
+);
 
-router.post('/end-ride',
+router.post(
+    '/end-ride',
     authMiddleware.authCaptain,
     body('rideId').isMongoId().withMessage('Invalid ride id'),
     rideController.endRide
-)
+);
 
 module.exports = router;
