484a040fecd3af4d16f6aa59abfa1e0717e127b4
pablobedoyab-boop <pablo.bedoyab@gmail.com>
2026-04-10 14:09:30 -0400

    Update ride.controller.js

--- backend/controllers/ride.controller.js
+++ backend/controllers/ride.controller.js
@@ -13,37 +13,38 @@ module.exports.createRide = async (req, res) => {
 
     const { pickup, destination, vehicle } = req.body;
 
-    // //console.log(req.user._id)
-
     try {
-        const ride = await rideService.createRide({ user: req.user._id, pickup, destination, vehicle });
-        res.status(201).json(ride);
+        const ride = await rideService.createRide({
+            user: req.user,
+            pickup,
+            destination,
+            vehicle
+        });
 
         const pickupCoordinates = await mapService.getAddressCoordinates(pickup);
 
-        const captainsInRadius = await mapService.getCaptainsInTheRadius(pickupCoordinates.ltd, pickupCoordinates.lng, 2);
-        // //console.log(captainsInRadius);
+        const captainsInRadius = await mapService.getCaptainsInTheRadius(
+            pickupCoordinates.ltd,
+            pickupCoordinates.lng,
+            2
+        );
 
-        ride.otp = ""
+        ride.otp = "";
 
         const rideWithUser = await rideModel.findOne({ _id: ride._id }).populate('user');
-        // //console.log(rideWithUser);
-        captainsInRadius.map(captain => {
 
+        captainsInRadius.map((captain) => {
             sendMessageToSocketId(captain.socketId, {
                 event: 'new-ride',
                 data: rideWithUser
-            })
-
-        })
+            });
+        });
 
         return res.status(201).json(ride);
     } catch (err) {
-
-        //console.log(err);
+        console.error('Error en createRide:', err);
         return res.status(500).json({ message: err.message });
     }
-
 };
 
 module.exports.getFare = async (req, res) => {
@@ -58,12 +59,12 @@ module.exports.getFare = async (req, res) => {
         const fare = await rideService.getFare(pickup, destination);
         return res.status(200).json(fare);
     } catch (err) {
+        console.error('Error en getFare:', err);
         return res.status(500).json({ message: err.message });
     }
-}
+};
 
 module.exports.confirmRide = async (req, res) => {
-    // //console.log("ride confirmation sent")
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -72,21 +73,22 @@ module.exports.confirmRide = async (req, res) => {
     const { rideId } = req.body;
 
     try {
-        const ride = await rideService.confirmRide({ rideId, captain: req.captain });
-        // //console.log(ride.user.socketId)
-        // //console.log(ride.user.socketId)
+        const ride = await rideService.confirmRide({
+            rideId,
+            captain: req.captain
+        });
+
         sendMessageToSocketId(ride.user.socketId, {
             event: 'ride-confirmed',
             data: ride
-        })
+        });
 
         return res.status(200).json(ride);
     } catch (err) {
-
-        //console.log(err);
+        console.error('Error en confirmRide:', err);
         return res.status(500).json({ message: err.message });
     }
-}
+};
 
 module.exports.startRide = async (req, res) => {
     const errors = validationResult(req);
@@ -97,20 +99,23 @@ module.exports.startRide = async (req, res) => {
     const { rideId, otp } = req.query;
 
     try {
-        const ride = await rideService.startRide({ rideId, otp, captain: req.captain });
-
-        // //console.log(ride);
+        const ride = await rideService.startRide({
+            rideId,
+            otp,
+            captain: req.captain
+        });
 
         sendMessageToSocketId(ride.user.socketId, {
             event: 'ride-started',
             data: ride
-        })
+        });
 
         return res.status(200).json(ride);
     } catch (err) {
+        console.error('Error en startRide:', err);
         return res.status(500).json({ message: err.message });
     }
-}
+};
 
 module.exports.endRide = async (req, res) => {
     const errors = validationResult(req);
@@ -121,15 +126,19 @@ module.exports.endRide = async (req, res) => {
     const { rideId } = req.body;
 
     try {
-        const ride = await rideService.endRide({ rideId, captain: req.captain });
-        // //console.log("almost final ", ride.user.socketId)
+        const ride = await rideService.endRide({
+            rideId,
+            captain: req.captain
+        });
+
         sendMessageToSocketId(ride.user.socketId, {
             event: 'ride-ended',
             data: ride
-        })
+        });
 
         return res.status(200).json(ride);
     } catch (err) {
+        console.error('Error en endRide:', err);
         return res.status(500).json({ message: err.message });
     }
-}
+};
