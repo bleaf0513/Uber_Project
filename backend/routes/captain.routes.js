204121a7697f9a759cc8533bd229dbe22592f28a
pablobedoyab-boop <pablo.bedoyab@gmail.com>
2026-04-11 16:19:37 -0400

    Update captain.routes.js

--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -6,23 +6,34 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register', [
     body('email').isEmail().withMessage('Please enter a valid email'),
-    body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-    body('vehicle.color').isLength({ min: 3 }).withMessage('Color must be at least 3 characters long'),
-    body('vehicle.plate').isLength({ min: 3 }).withMessage('Plate must be at least 3 characters long'),
-    body('vehicle.capacity').isInt({ min: 1 }).withMessage('Capacity must be at least 1'),
-    body('vehicle.vehicleType').isIn(['car', 'motorcycle', 'auto']).withMessage('Please enter a valid vehicle type'),
+    body('fullname.firstname')
+        .isLength({ min: 3 })
+        .withMessage('First name must be at least 3 characters long'),
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
+    body('vehicle.color')
+        .isLength({ min: 3 })
+        .withMessage('Color must be at least 3 characters long'),
+    body('vehicle.plate')
+        .isLength({ min: 3 })
+        .withMessage('Plate must be at least 3 characters long'),
+    body('vehicle.capacity')
+        .isInt({ min: 1 })
+        .withMessage('Capacity must be at least 1'),
+    body('vehicle.vehicleType')
+        .isIn(['motorcycle', 'car', 'light_cargo', 'van', 'truck'])
+        .withMessage('Please enter a valid vehicle type'),
 ], (req, res) => {
-    // //console.log("Registering captain");
     captainController.registerCaptain(req, res);
 });
 
-
 router.post('/login', [
     body('email').isEmail().withMessage('Please enter a valid email'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
 ], (req, res) => {
-    // //console.log("Logging in captain");
     captainController.loginCaptain(req, res);
 });
 
