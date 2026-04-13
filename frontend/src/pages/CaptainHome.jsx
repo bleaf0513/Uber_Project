e2104f0e1980f034537f571bfa2507bcb7082b45
pablobedoyab-boop <pablo.bedoyab@gmail.com>
2026-04-13 00:59:37 -0400

    Update CaptainHome.jsx

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -157,7 +157,7 @@ const CaptainHome = () => {
       <div className="absolute top-0 left-0 ml-7 py-7 z-30">
         <Link to="/">
           <img
-            className="absolute w-20 ml-7 pt-7 z-30"
+            className="w-20"
             src="/logo-centralgo.png"
             alt="Central Go"
           />
